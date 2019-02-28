4aca24b0aeb29c5804cb63975f4c5497cb784e30
Rick Strafy <rixafy@gmail.com>
2019-02-28 23:33:18 +0100

    Add PublishableTrait

---
+++ src/PublishableTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rixafy\DoctrineTraits;
+
+trait PublishableTrait
+{
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    private $is_published = true;
+
+    /**
+     * @return bool
+     */
+    public function isPublished(): bool
+    {
+        return $this->is_published;
+    }
+
+    public function publish(): void
+    {
+        $this->is_published = true;
+    }
+
+    public function hide(): void
+    {
+        $this->is_published = false;
+    }
+}
