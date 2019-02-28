d24a894163e398433a44f4ec0d2af6f7034dfd05
Rick Strafy <rixafy@gmail.com>
2019-02-28 23:28:46 +0100

    Add RemovableTrait

---
+++ src/RemovableTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rixafy\DoctrineTraits;
+
+trait RemovableTrait
+{
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    private $is_removed = true;
+
+    /**
+     * @return bool
+     */
+    public function isRemoved(): bool
+    {
+        return $this->is_removed;
+    }
+
+    public function remove(): void
+    {
+        $this->is_removed = true;
+    }
+
+    public function restore(): void
+    {
+        $this->is_removed = false;
+    }
+}
